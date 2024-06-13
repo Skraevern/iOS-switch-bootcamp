38880668c22ffecf30d38172686578523b49f2ae
Skraevern <86933999+Skraevern@users.noreply.github.com>
2024-06-13 19:53:08 +0200

    38

--- 03-swift-basics/Dicee-iOS13/Dicee-iOS13/ViewController.swift
+++ 03-swift-basics/Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 let dice1 = #imageLiteral(resourceName: "DiceOne"), dice2 = #imageLiteral(resourceName: "DiceTwo"), dice3 = #imageLiteral(resourceName: "DiceThree"), dice4 = #imageLiteral(resourceName: "DiceFour"), dice5 = #imageLiteral(resourceName: "DiceFive"), dice6 = #imageLiteral(resourceName: "DiceSix")
 
+let diceArray = [dice1, dice2, dice3, dice4, dice5, dice6]
+
 class ViewController: UIViewController {
 
     // IBOutlet allows me to reference a UI elemnt
@@ -19,13 +21,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        //  WHO        WHAT    VALUE
-        diceImageView1.image = dice3
-        diceImageView1.alpha = 0.5
-        diceImageView2.image = dice2
+        //  WHO           WHAT    VALUE
+        // diceImageView1.image = dice6
+        // diceImageView1.alpha = 0.5 50% see through
+        // diceImageView2.image = dice2
+        
+        diceImageView1.image = diceArray.randomElement()
+        diceImageView2.image = diceArray.randomElement()
+        
         
     }
-
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) { //print("Button got tapped.")
+        //diceImageView1.image = dice4
+        //diceImageView2.image = dice4
+        
+        diceImageView1.image = diceArray.randomElement()
+        diceImageView2.image = diceArray.randomElement()
+    }
+    
 
 }
 
