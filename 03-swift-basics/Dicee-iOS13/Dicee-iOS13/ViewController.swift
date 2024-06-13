aecf12d9dc1c727e77aa03a7e82c8f1c0de8c551
Skraevern <86933999+Skraevern@users.noreply.github.com>
2024-06-13 20:17:24 +0200

    41

--- 03-swift-basics/Dicee-iOS13/Dicee-iOS13/ViewController.swift
+++ 03-swift-basics/Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -8,13 +8,16 @@
 
 import UIKit
 
-let dice1 = #imageLiteral(resourceName: "DiceOne"), dice2 = #imageLiteral(resourceName: "DiceTwo"), dice3 = #imageLiteral(resourceName: "DiceThree"), dice4 = #imageLiteral(resourceName: "DiceFour"), dice5 = #imageLiteral(resourceName: "DiceFive"), dice6 = #imageLiteral(resourceName: "DiceSix")
-
-let diceArray = [dice1, dice2, dice3, dice4, dice5, dice6]
+let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
 
 class ViewController: UIViewController {
+    
+    // # 41
+    // var leftDiceNum = 0
+    // var rightDiceNum = 5
 
     // IBOutlet allows me to reference a UI elemnt
+    
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
@@ -26,6 +29,10 @@ class ViewController: UIViewController {
         // diceImageView1.alpha = 0.5 50% see through
         // diceImageView2.image = dice2
         
+        // #41
+        // diceImageView1.image = diceArray[leftDiceNum]
+        // diceImageView2.image = diceArray[rightDiceNum]
+        
         diceImageView1.image = diceArray.randomElement()
         diceImageView2.image = diceArray.randomElement()
         
@@ -35,6 +42,13 @@ class ViewController: UIViewController {
     @IBAction func rollButtonPressed(_ sender: UIButton) { //print("Button got tapped.")
         //diceImageView1.image = dice4
         //diceImageView2.image = dice4
+        
+        // #41
+        // leftDiceNum += 1
+        // rightDiceNum -= 1
+        // diceImageView1.image = diceArray[leftDiceNum]
+        // diceImageView2.image = diceArray[rightDiceNum]
+        
         
         diceImageView1.image = diceArray.randomElement()
         diceImageView2.image = diceArray.randomElement()
