31711d7f451d9541ad5ffb619ee197938fc34549
Skraevern <86933999+Skraevern@users.noreply.github.com>
2024-06-15 16:20:52 +0200

    Deep dive data types

---
+++ 00-swift-deep-dives/03-constants.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+let a = 3
+var b = 2
+
+a = 5
+b = 3
---
+++ 00-swift-deep-dives/04-data-types.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+// String
+let myName1 = "Angela"
+let myName2 = "My name is Angela"
+
+// Int (Integer)
+let integer = 12
+let longInt = 1234567891011121314 //Max numbers is 19
+let toLongInt = 12345678910111213141
+let negativeInt = -134
+
+// Float (Decimals)
+let float = 12.345
+let longFloat = 1.2345678
+
+// Double (Long Decimals
+let double = 1.12312312412412412412412412124214124124214221412
+
+// Bool (Boolean)
+let bool1 = true
+let bool2 = false
+
+
