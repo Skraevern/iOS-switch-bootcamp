e08a171a3c1c744863195e1725a045679c6cfcac
Skraevern <86933999+Skraevern@users.noreply.github.com>
2024-06-16 20:04:42 +0200

    Finished Xylophone

--- 07-using-and-understanding-documentation/Xylophone-iOS13/Xylophone/ViewController.swift
+++ 07-using-and-understanding-documentation/Xylophone-iOS13/Xylophone/ViewController.swift
@@ -7,15 +7,53 @@
 //
 
 import UIKit
+import AVFoundation
+
+
+class Test {
+    
+    var player: AVAudioPlayer!
+    
+
+    
+}
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer!
+    
+    func playAudio(note: String) {
+        
+        let url = Bundle.main.url(forResource: note, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
-    
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        // C
+        playAudio(note: "C")
+    }
+    @IBAction func dButton(_ sender: UIButton) {
+        playAudio(note: "D")
+    }
+    @IBAction func eButton(_ sender: UIButton) {
+        playAudio(note: "E")
+    }
+    @IBAction func fButton(_ sender: UIButton) {
+        playAudio(note: "F")
+    }
+    @IBAction func gButton(_ sender: UIButton) {
+        playAudio(note: "G")
+    }
+    @IBAction func aButton(_ sender: UIButton) {
+        playAudio(note: "A")
+    }
+    @IBAction func bButton(_ sender: UIButton) {
+        playAudio(note: "B")
+    }
 }
 
