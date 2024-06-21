d960d2f4d9bd4421294fcdf8f78fc36d5e4fbb09
Skraevern <86933999+Skraevern@users.noreply.github.com>
2024-06-21 20:26:43 +0200

    75

--- 07-using-and-understanding-documentation/Xylophone-iOS13/Xylophone/ViewController.swift
+++ 07-using-and-understanding-documentation/Xylophone-iOS13/Xylophone/ViewController.swift
@@ -9,15 +9,6 @@
 import UIKit
 import AVFoundation
 
-
-class Test {
-    
-    var player: AVAudioPlayer!
-    
-
-    
-}
-
 class ViewController: UIViewController {
     
     var player: AVAudioPlayer!
@@ -32,27 +23,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-    @IBAction func cButton(_ sender: Any) {
-        playAudio(note: "C")
-    }
-    @IBAction func dButton(_ sender: UIButton) {
-        playAudio(note: "D")
-    }
-    @IBAction func eButton(_ sender: UIButton) {
-        playAudio(note: "E")
-    }
-    @IBAction func fButton(_ sender: UIButton) {
-        playAudio(note: "F")
-    }
-    @IBAction func gButton(_ sender: UIButton) {
-        playAudio(note: "G")
-    }
-    @IBAction func aButton(_ sender: UIButton) {
-        playAudio(note: "A")
-    }
-    @IBAction func bButton(_ sender: UIButton) {
-        playAudio(note: "B")
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+        let buttonTitle = sender.currentTitle!
+        playAudio(note: buttonTitle)
     }
 }
-
