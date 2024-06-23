6701855a1e2d0d97b3f7c3e6121f6a45ac079bed
Skraevern <86933999+Skraevern@users.noreply.github.com>
2024-06-23 19:26:55 +0200

    Deep dives dictionaries

---
+++ 00-swift-deep-dives/11-dictionaries.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var dict: [String: Any] = [
+    "Brewery": "A place where beer is made",
+    "Bakery": "A place where bread is made",
+    "Angela": 123123123,
+    "Philiip": 123123123123
+]
+
+print(dict["Bakery"]!)
+print(dict)
+
+// ["Brewery": "A place where beer is made", "Angela": 123123123, "Philiip": 123123123123, "Bakery": "A place where bread is made"]
