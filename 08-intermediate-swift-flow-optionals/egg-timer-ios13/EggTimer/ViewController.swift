def118ba91a01560a595a8e63e721703ffe6f71e
Skraevern <86933999+Skraevern@users.noreply.github.com>
2024-06-25 20:32:22 +0200

    88

--- 08-intermediate-swift-flow-optionals/egg-timer-ios13/EggTimer/ViewController.swift
+++ 08-intermediate-swift-flow-optionals/egg-timer-ios13/EggTimer/ViewController.swift
@@ -11,20 +11,41 @@ import UIKit
 class ViewController: UIViewController {
     
     let eggTimes = [
-        "Soft": 5,
-        "Medium": 7,
-        "Hard": 12,
+        "Soft": 10, // 5 min == 300
+        "Medium": 420, // 7 min == 420
+        "Hard": 720, // 12 min == 720
     ]
+    var secondsRemaining: Int? = nil
+    var timerActive = false
+    
 
     @IBAction func hardnessSelected(_ sender: UIButton) {
         let hardness = sender.currentTitle!
         
         if hardness == "Soft" {
-            print(eggTimes["Soft"]!)
+            secondsRemaining = eggTimes["Soft"]!
         } else if hardness == "Medium"{
-            print(eggTimes["Medium"]!)
+            secondsRemaining = eggTimes["Medium"]!
         } else {
-            print(eggTimes["Hard"]!)
+            secondsRemaining = eggTimes["Hard"]!
+        }
+        
+        if timerActive {
+            secondsRemaining = 0
+        }
+        else {
+            timerActive = true
+
+            Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
+                if self.secondsRemaining! > 0 {
+                    print ("\(String(describing: self.secondsRemaining!)) seconds")
+                    self.secondsRemaining! -= 1
+                } else {
+                    print("Countdown finished")
+                    self.timerActive = false
+                    Timer.invalidate()
+                }
+            }
         }
     }
 }
