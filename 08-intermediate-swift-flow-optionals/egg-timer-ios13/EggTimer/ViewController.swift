ca307af56adc2748ded78fec4b9fbe230cb2ec70
Skraevern <86933999+Skraevern@users.noreply.github.com>
2024-06-29 15:29:54 +0200

    Revert "92"
    This reverts commit 64ebb370e0f8212bb9f1d43dece58e4cfb9691ab.

--- 08-intermediate-swift-flow-optionals/egg-timer-ios13/EggTimer/ViewController.swift
+++ 08-intermediate-swift-flow-optionals/egg-timer-ios13/EggTimer/ViewController.swift
@@ -15,44 +15,36 @@ class ViewController: UIViewController {
         "Medium": 420, // 7 min == 420
         "Hard": 720, // 12 min == 720
     ]
-    var timer = Timer()
-    var secondsRemaining = 0
+    var timerActive = false
     var secondsPast = 0
     var totalTime = 0
-    var hardness = ""
     
     @IBOutlet weak var progressBar: UIProgressView!
-    @IBOutlet weak var titleLabel: UILabel!
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        timer.invalidate()
-        hardness = sender.currentTitle!
+        let hardness = sender.currentTitle!
         totalTime = eggTimes[hardness]!
-        secondsRemaining = totalTime
         
-        progressBar.progress = 0.0
-        secondsPast = 0
-        updateTitleLabel(title: hardness)
-        
-        timer = Timer.scheduledTimer(timeInterval: 1.0, target:self, selector: #selector(updateTimer), userInfo:nil, repeats: true)
-    }
-    
-    @objc func updateTimer() {
-        if self.secondsPast < self.totalTime {
-            self.secondsPast += 1
-            self.secondsRemaining -= 1
-            self.progressBar.progress = Float(self.secondsPast) / Float(self.totalTime)
-            updateTitleLabel(title: self.hardness)
-        } else {
-            timer.invalidate()
-            updateTitleLabel(title: "DONE!")
+        if timerActive {
+            secondsPast = totalTime
         }
-    }
-    @objc func updateTitleLabel(title: String) {
-        let minutes = self.secondsRemaining / 60
-        let seconds = self.secondsRemaining % 60
+        else {
+            timerActive = true
 
-        let timeString = String(format: "%02d:%02d", minutes, seconds)
-        titleLabel.text = "\(title)\n\n\(timeString)"
+            Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
+                if self.secondsPast <= self.totalTime {
+                    let percentageRemaining = Float(self.secondsPast) / Float(self.eggTimes[hardness]!)
+                    print(percentageRemaining)
+                    self.progressBar.progress = percentageRemaining
+                    
+                    self.secondsPast += 1
+                    
+                } else {
+                    print("Countdown finished")
+                    self.timerActive = false
+                    Timer.invalidate()
+                }
+            }
+        }
     }
 }
