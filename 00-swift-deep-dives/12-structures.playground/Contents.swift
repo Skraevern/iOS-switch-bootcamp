b5b443461bd00f0857c1fc235bec3879b6bc12a5
Skraevern <86933999+Skraevern@users.noreply.github.com>
2024-06-30 14:11:07 +0200

    Deep Dives Struct

---
+++ 00-swift-deep-dives/12-structures.playground/Contents.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+// Structures = custom data types
+struct MyStruct {   } // Capitalize first. Like Int, Float, String
+
+struct Town {
+    let name = "Angelaland"
+    var citizens = ["Angela", "Jack Bauer", "JimJohnny"]
+    var resources = ["Grain": 100, "Ore": 42, "Wool": 75]
+    
+    func fortify() {
+        print("Defences increased!")
+    }
+}
+var myTown = Town()
+
+print(myTown.self)
+// Town(name: "Angelaland", citizens: ["Angela", "Jack Bauer", "JimJohnny"], resources: ["Grain": 100, "Ore": 42, "Wool": 75])
+
+print(myTown.citizens)
+// ["Angela", "Jack Bauer", "JimJohnny"]
+
+print(myTown.name)
+// Angelaland
+
+print(myTown.resources)
+// ["Grain": 100, "Ore": 42, "Wool": 75]
+
+print("\(myTown.name) has \(myTown.resources["Grain"]!) bags of grain.")
+// Angelaland has 100 bags of grain.
+
+myTown.citizens.append("Keanu Reeves")
+print(myTown.citizens.count)
+// 4
+
+myTown.fortify()
+// Defences increased!
+
+myTown.name = "Change"
