13909da057a6bfafb823b59cb4a536b840c54be8
86933999+Skraevern@users.noreply.github.com <skraevern@Kristians-Mac-mini.local>
2024-07-09 13:23:36 +0200

    134

---
+++ 12-tipsy-tip-calculator/Tipsy/Controllers/CalculatorViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Angela Yu on 09/09/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+    
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPctBtn: UIButton!
+    @IBOutlet weak var tenPctBtn: UIButton!
+    @IBOutlet weak var twentyPctBtn: UIButton!
+    @IBOutlet weak var splitNumLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func tipChanged(_ sender: UIButton) {
+
+    }
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+
+    }
+    @IBAction func calculateBtnPressed(_ sender: UIButton) {
+
+    }
+    
+}
